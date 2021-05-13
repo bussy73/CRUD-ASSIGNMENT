adf63d1f799f9bfb78c656744b71d7c83200a99f
Osinachi <oukomadu@gmail.com>
2021-05-13 07:48:43 +0100

    heroku changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const connectDB = require("./db");
 const officerRoutes = require('./routes/officerRoutes');
 require('dotenv').config();
-const { PORT } = process.env.PORT ||(5000)
+const { PORT } = process.env.PORT;
 
 
 connectDB();
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
     res.status(200).json({ message: 'Welcome to Zuri Training!' });
 });
 
-const port = process.env.PORT || PORT;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`app running on port ${port}`);
